cdb122877c3881041cf7cf55c6de9cda604f7c08
William <wyskanashiro@gmail.com>
2022-03-16 09:53:16 -0400

    Listing

--- app/src/main/java/will/shiro/composelabs1/MainActivity.kt
+++ app/src/main/java/will/shiro/composelabs1/MainActivity.kt
@@ -3,6 +3,7 @@ package will.shiro.composelabs1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -17,10 +18,16 @@ import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -30,12 +37,14 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import will.shiro.composelabs1.ui.theme.Composelabs1Theme
 import will.shiro.composelabs1.ui.theme.PrimaryH4
+import kotlin.math.exp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,96 +59,39 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
-    var shouldShowOnboarding by remember { mutableStateOf(false) }
-    val listState = rememberLazyListState()
-
-    if (shouldShowOnboarding) {
-        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
-    } else {
-        Greetings(listState = listState)
-    }
-}
-
-@Composable
-fun OnboardingScreen(onContinueClicked: () -> Unit) {
-
-    Surface {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text("Welcome to the Basics Codelab!")
-            Button(
-                modifier = Modifier.padding(vertical = 24.dp),
-                onClick = onContinueClicked
-            ) {
-                Text("Continue", style = PrimaryH4)
-            }
+    LazyColumn {
+        items(List(200) { it }) {
+            Item(it)
         }
     }
 }
 
 @Composable
-private fun Greetings(names: List<String> = List(1000) { "$it" }, listState: LazyListState) {
-    LazyColumn(state = listState) {
-        items(items = names) { name ->
-            Greeting(name = name, listState = listState)
-        }
-    }
-}
-
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
-@Composable
-fun OnboardingPreview() {
-    Composelabs1Theme {
-        OnboardingScreen(onContinueClicked = {})
+fun Item(number: Int) {
+    Card(backgroundColor = MaterialTheme.colors.primary, modifier = Modifier.padding(16.dp)) {
+        ItemContent(number)
     }
 }
 
 @Composable
-private fun Greeting(name: String, listState: LazyListState) {
-    val coroutineScope = rememberCoroutineScope()
-    val expanded = rememberSaveable { mutableStateOf(false) }
-    val extraPadding by animateDpAsState(
-        targetValue = if (expanded.value) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
+fun ItemContent(number: Int) {
+    val expanded = remember { mutableStateOf(false) }
 
-    Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                Text(text = "Hello, ")
-                Text(text = name, style = PrimaryH4)
+    Column(Modifier.padding(16.dp).animateContentSize()) {
+        Row {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = stringResource(id = R.string.hello))
+                Text(text = number.toString(), style = MaterialTheme.typography.h4)
             }
-            OutlinedButton(
-                onClick = {
-                    expanded.value = !expanded.value
-//                    coroutineScope.launch {
-//                        listState.scrollToItem(100)
-//                    }
-                }
-            ) {
-                Text(if (expanded.value) "Show less" else "Show more")
+            IconButton(onClick = { expanded.value = !expanded.value }) {
+                Icon(
+                    imageVector = if (expanded.value) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if (expanded.value) stringResource(R.string.show_less) else stringResource(R.string.show_more)
+                )
             }
         }
-    }
-}
-
-@Preview(showBackground = true, widthDp = 320)
-@Composable
-fun DefaultPreview() {
-    Composelabs1Theme {
-//        Greetings()
+        if (expanded.value) {
+            Text(text = stringResource(R.string.long_text))
+        }
     }
 }
