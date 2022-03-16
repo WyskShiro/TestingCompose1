fa0609f53d33db7bc1b561c871cb9970893e4a34
William <wyskanashiro@gmail.com>
2022-03-16 10:05:24 -0400

    Adding constraint layout

--- app/src/main/java/will/shiro/composelabs1/MainActivity.kt
+++ app/src/main/java/will/shiro/composelabs1/MainActivity.kt
@@ -4,47 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
+import androidx.constraintlayout.compose.ConstraintLayout
 import will.shiro.composelabs1.ui.theme.Composelabs1Theme
-import will.shiro.composelabs1.ui.theme.PrimaryH4
-import kotlin.math.exp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -77,21 +56,51 @@ fun Item(number: Int) {
 fun ItemContent(number: Int) {
     val expanded = remember { mutableStateOf(false) }
 
-    Column(Modifier.padding(16.dp).animateContentSize()) {
-        Row {
-            Column(modifier = Modifier.weight(1f)) {
-                Text(text = stringResource(id = R.string.hello))
-                Text(text = number.toString(), style = MaterialTheme.typography.h4)
-            }
-            IconButton(onClick = { expanded.value = !expanded.value }) {
-                Icon(
-                    imageVector = if (expanded.value) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
-                    contentDescription = if (expanded.value) stringResource(R.string.show_less) else stringResource(R.string.show_more)
-                )
-            }
+    ConstraintLayout(
+        Modifier
+            .padding(16.dp)
+            .animateContentSize()
+            .fillMaxWidth()
+    ) {
+        val (helloText, numberText, iconButton, longText) = createRefs()
+
+        Text(
+            modifier = Modifier.constrainAs(helloText) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+            },
+            text = stringResource(id = R.string.hello)
+        )
+
+        Text(
+            modifier = Modifier.constrainAs(numberText) {
+                top.linkTo(helloText.bottom)
+                start.linkTo(parent.start)
+            },
+            text = number.toString(), style = MaterialTheme.typography.h4
+        )
+
+        IconButton(
+            modifier = Modifier.constrainAs(iconButton) {
+                top.linkTo(parent.top)
+                end.linkTo(parent.end)
+            },
+            onClick = { expanded.value = !expanded.value }
+        ) {
+            Icon(
+                imageVector = if (expanded.value) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded.value) stringResource(R.string.show_less) else stringResource(R.string.show_more)
+            )
         }
         if (expanded.value) {
-            Text(text = stringResource(R.string.long_text))
+            Text(
+                modifier = Modifier.constrainAs(longText) {
+                    top.linkTo(numberText.bottom)
+                    end.linkTo(parent.end)
+                    start.linkTo(parent.start)
+                },
+                text = stringResource(R.string.long_text)
+            )
         }
     }
 }
